9211f6c7481eb2b939e1b9a125477e56430473a0
ALBIN BENNY <albin99.in@gmail.com>
2024-01-28 10:54:49 +0530

    Account activation mail

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -144,3 +144,9 @@ MESSAGE_TAGS = {
     messages.ERROR:'danger',
     
 }
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'albin99.in@gmail.com'
+EMAIL_HOST_PASSWORD = 'ersn cdzm uzbz tdhi'
